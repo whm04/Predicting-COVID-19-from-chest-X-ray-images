a9db5a6cf3f18a592bd73394e67ae4b684b44653
whm04 <46822616+whm04@users.noreply.github.com>
2020-04-15 10:20:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 def homepage():
     
 
-    return render_template('index.html')
+    return render_template('index.html', title = "Welcome", paragraph = 'Lorem ipsum dolor sit amet')
  
 
 if __name__ == '__main__':
