8542f56c5467bbe9a1902422b699736a8d52d8e7
whm04 <46822616+whm04@users.noreply.github.com>
2020-04-15 03:16:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
 
 app = Flask(__name__)
+import os
+
+port = int(os.environ.get('PORT', 5000))
+
 
 @app.route('/')
 def homepage():
@@ -10,4 +14,4 @@ def homepage():
  
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
+    app.run(host='0.0.0.0', port=port, debug=True)
